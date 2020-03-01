51ff75cef2b6b6f6a22e9b567ae08f69d2b54d32
Yerkhanat <erha.mts@gmail.com>
2020-02-29 16:53:15 -0800

    modified SpringPractice class

--- src/day01/SpringPractice.java
+++ src/day01/SpringPractice.java
@@ -3,5 +3,9 @@ package day01;
 public class SpringPractice {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        String str = "Java is fun";
+        System.out.println(str);
+
+
     }
 }
