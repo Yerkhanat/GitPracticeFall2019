885988f8ffba0dc6241834fc31ae9a1c635ff228
Yerkhanat <erha.mts@gmail.com>
2020-03-11 17:13:26 -0700

    added new item to Array list

--- src/day01/Colors.java
+++ src/day01/Colors.java
@@ -2,7 +2,8 @@ package day01;
 
 public class Colors {
     public static void main(String[] args) {
-        String[] colors = {"red","blue","green"};
+        String[] colors = {"red","blue","green","yellow"};
+
 
     }
 }
